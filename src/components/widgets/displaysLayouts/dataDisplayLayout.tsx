61b48440401458ee6c31e112a174163bf55e9102
Hyde <francoiskhamsing@gmail.com>
2025-03-21 22:11:20 +0000

    feat: add data display components and styling

--- src/components/widgets/displaysLayouts/dataDisplayLayout.tsx
+++ src/components/widgets/displaysLayouts/dataDisplayLayout.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import styles from "./dataDisplay.module.css";
+import { AlignCenter } from "lucide-react";
 
 type DataDisplayLayoutProps = {
   header?: string;
@@ -7,12 +9,40 @@ type DataDisplayLayoutProps = {
 
 const DataDisplayLayout = ({ header, data }: DataDisplayLayoutProps) => {
   return (
-    <div className={styles.displayContainer}>
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0 }}
+      className={styles.displayContainer}
+    >
       <div className={styles.display}>
-        <h3>{header}</h3>
-        <h3>{data}</h3>
+        <motion.h3
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          style={{ textAlign: "center" }}
+          key={header}
+        >
+          {header?.split("").map((char, index) => (
+            <motion.span
+              key={`${char}-${index}`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 0.1,
+                delay: index * 0.05,
+                ease: "easeInOut",
+              }}
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.h3>
+        <motion.h3 initial={{ opacity: 0 }} animate={{ opacity: 1 }} key={data}>
+          {parseFloat(data).toFixed(2)}
+        </motion.h3>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
